9229f99104f52e96594028c65c7614b21408b179
Christopher Schmidt <csarmyb13@gmail.com>
2023-05-20 21:46:27 -0500

    Update sales_data.php

--- Front-End/mystore/sales_data.php
+++ Front-End/mystore/sales_data.php
@@ -4,7 +4,7 @@ header('Content-Type: application/json');
 
 define('DB_HOST', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'Ctmdsbggpn900!!');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'sneakers');
 
 $mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
